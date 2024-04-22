ec865ccdd1a52e18b27c64986976f213499ddee5
shajigholam <shajigholam@myseneca.ca>
2024-04-22 11:36:00 -0400

    signup & hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +14,25 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// when we create molels with mongoose, they automatically come with methods like create, find, findOne and delete that we can use. we also can make our own
+// because we are using this.findOne arrow function won't work and we should use regualr function
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    //here we should use this instead of User
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // hashing password (by bcrypt package)
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    // add the record(doc) to the db
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
