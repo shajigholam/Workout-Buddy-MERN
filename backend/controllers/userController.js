fe9172ec5dae6c9914500966a0cac43c3ca6df03
shajigholam <shajigholam@myseneca.ca>
2024-04-22 18:27:00 -0400

    Added login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,19 @@ const cerateToken = (_id) => {
 
 // login user 
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = cerateToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+    // res.json({mssg: 'login user'})
 }
 
 // signup user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,8 +16,9 @@ const userSchema = new Schema({
     }
 })
 
-// when we create molels with mongoose, they automatically come with methods like create, find, findOne and delete that we can use. we also can make our own
+// when we create models with mongoose, they automatically come with methods like create, find, findOne and delete that we can use. we also can make our own as follows
 // because we are using this.findOne arrow function won't work and we should use regualr function
+
 // static signup method
 userSchema.statics.signup = async function(email, password) {
 
@@ -32,7 +33,7 @@ userSchema.statics.signup = async function(email, password) {
     if (!validator.isStrongPassword(password)) {
         throw Error('Password not strong enough')
     }
-    
+
     //here we should use this instead of User
     const exists = await this.findOne({ email })
 
@@ -45,6 +46,26 @@ userSchema.statics.signup = async function(email, password) {
     const hash = await bcrypt.hash(password, salt)
     // add the record(doc) to the db
     const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Incorrect Email')
+    }
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect Password')
+    }
 
     return user
 }
