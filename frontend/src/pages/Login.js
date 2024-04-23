8cbd00833e9e8315c485dbef1a14e22665dde1b9
shajigholam <shajigholam@myseneca.ca>
2024-04-23 19:09:47 -0400

    Added useLogin

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, isLoading, error } = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
+        // console.log(email, password)
     }
 
     return (
@@ -30,7 +33,8 @@ const Login = () => {
                 value={password}
             />
 
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
 
         </form>
     )
