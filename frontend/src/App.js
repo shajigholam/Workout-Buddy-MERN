25d24fb1b25ac6774d70040089ab1ac3effe2317
shajigholam <shajigholam@myseneca.ca>
2024-04-22 23:59:31 -0400

    added signup and login routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages and components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -14,6 +16,14 @@ function App() {
             <Route 
               path="/"
               element={<Home />}
+            />
+            <Route 
+              path="/login"
+              element={<Login />}
+            />
+            <Route 
+              path="/signup"
+              element={<Signup />}
             />
           </Routes>
         </div>
